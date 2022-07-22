2b32a45542cd373797ca05fe59c3e10f90d97c03
Vladislava-qi <106888634+Vladislava-qi@users.noreply.github.com>
2022-07-22 10:04:28 +0300

    17 задание

---
+++ reg_obr.php
@@ -0,0 +1,12 @@
+<?php
+$name = $_POST['name'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+
+echo "<hr>";
+echo "Имя: $name<br>
+Фамилия: $lastname<br>
+E-mail: $email<br>
+Пароль: $pass";
+?> 
